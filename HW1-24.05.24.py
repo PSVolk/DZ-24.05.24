5c61d3a2ff548b7f1b7a38abfc2a1031345952d1
PSVolk <159309202+PSVolk@users.noreply.github.com>
2024-05-14 21:16:30 +0500

    Add files via upload

---
+++ HW1-24.05.24.py
@@ -0,0 +1,5 @@
+def task1():
+    a = "\"Don\'t compare yourself with anyone in this world…\n if you do so, you are insulting yourself.\""
+    b = "Bill Gates"
+    return f"{a}\n {b:>48}"
+print(task1())
